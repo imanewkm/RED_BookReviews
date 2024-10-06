6594a34985aa67a39cea8908c5deb515fdeba17d
imanewkm <imanewkm02@gmail.com>
2024-10-06 15:38:25 +0100

    login for registered users

---
+++ server/router/auth_user.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+let books = require("./booksdb.js");
+const regd_users = express.Router();
+require('dotenv').config();
+
+let users = [];
+
+// Helper function for token validation
+const verifyToken = (token) => jwt.verify(token, process.env.JWT_SECRET || 'your_jwt_secret');
+
+// login for registered users
+regd_users.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({ message: "Username and password are required "});
+    }
+
+    if (!authenticatedUser(username, password)) {
+        return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    const token = jwt.sign({ username }, process.env.JWT_SECRET || 'your_jwt_secret', {expiresIn: '1hr' });
+    return res.status(200).json({ message: "Login successful", token });
+});
