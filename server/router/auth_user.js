21c391db4b28977774558821a5a5f5d97aa3f23a
imanewkm <imanewkm02@gmail.com>
2024-10-06 15:45:10 +0100

    Add review with token verification

--- server/router/auth_user.js
+++ server/router/auth_user.js
@@ -9,6 +9,8 @@ let users = [];
 // Helper function for token validation
 const verifyToken = (token) => jwt.verify(token, process.env.JWT_SECRET || 'your_jwt_secret');
 
+// User registration ???
+
 // login for registered users
 regd_users.post("/login", (req, res) => {
     const { username, password } = req.body;
@@ -23,4 +25,24 @@ regd_users.post("/login", (req, res) => {
 
     const token = jwt.sign({ username }, process.env.JWT_SECRET || 'your_jwt_secret', {expiresIn: '1hr' });
     return res.status(200).json({ message: "Login successful", token });
+});
+
+// Add a review with token verification
+regd_users.put("/auth/review/:isbn", (req, res) => {
+    const { isbn } = req.params;
+    const { review } = req.body;
+    const token = req.headers['authorization'];
+
+    if (!token) return res.status(401).json({ message: "Token is required" });
+
+    try {
+        const { username } = verifyToken(token);
+
+        if (!books[isbn].review) books[isbn].reviews = {};
+        books[isbn].reviews[username] = review;
+
+        return res.status(200).json({ message: "Review added/modified successfully" });
+    } catch (err) {
+        return res.status(401).json({ message: "Invalide token" });
+    }
 });
