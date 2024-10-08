c1258f337df7e12aaafd0ae5a340d76c6302fb9b
imanewkm <imanewkm02@gmail.com>
2024-10-08 12:37:27 +0100

    Getting book details

--- server/router/general.js
+++ server/router/general.js
@@ -20,3 +20,48 @@ public_users.post("/register", async (req, res) => {
 
     return res.status(201).json({ message: "User registered successfully" });
 });
+
+// Get the list of books available
+generalRouter.get('/books', (req, res) => {
+    try {
+        res.status(200).json(books);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching books', error: error.message });
+    }
+});
+
+// Get book details by ISBN
+generalRouter.get('/books/isbn/:isbn', (req, res) => {
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+
+    if (book) {
+        res.status(200).json(book);
+    } else {
+        res.status(404).json({ message: "Book not found" });
+    }
+});
+
+// Get book details by author
+generalRouter.get('/books/author/:author', (req, res) => {
+    const author = req.params.author;
+    const booksByAuthor = Object.values(books).filter(book => book.author === author);
+
+    if (booksByAuthor.length > 0) {
+        res.status(200).json(booksByAuthor);
+    } else {
+        res.status(404).json({ message: "No books found by this author" });
+    }
+});
+
+// Get book details by title
+generalRouter.get('/books/title/:title', (req, res) => {
+    const title = req.params.title;
+    const booksByTitle = Object.values(books).filter(book => book.title === title);
+
+    if (booksByTitle.length > 0) {
+        res.status(200).json(booksByTitle);
+    } else {
+        res.status(404).json({ message: "No books found with this title" });
+    }
+});
