266519dbbd6291aa13bb413d721f5f26e130415c
imanewkm <imanewkm02@gmail.com>
2024-10-12 22:37:24 +0100

    Remove /models directory - Switched to Google Sheets Database

--- server/models/usersdb.js
+++
@@ -1,9 +0,0 @@
-const { getUsers } = require('../services/googleSheetService');
-
-let users = [];
-
-getUsers().then(data => {
-    users = data;
-}).catch(err => console.log('Error landing users:', err));
-
-module.exports = { users };
